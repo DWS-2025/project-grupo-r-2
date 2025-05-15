5f5c8da6535590fe890cb61c842ea3309b785aed
Cesarmb3 <146852522+Cesarmb3@users.noreply.github.com>
2025-05-15 20:40:08 +0200

    config

--- SpartanWrath/src/main/java/com/spartanwrath/security/SecurityConfig.java
+++ SpartanWrath/src/main/java/com/spartanwrath/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,7 +31,7 @@ import static org.springframework.security.web.header.writers.ClearSiteDataHeade
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
 	@Autowired
 	RepositoryUserDetailsService detailService;
@@ -47,80 +48,91 @@ public class SecurityConfig{
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
 		return authConfig.getAuthenticationManager();
 	}
 
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
 		authProvider.setUserDetailsService(detailService);
 		authProvider.setPasswordEncoder(passwordEncoder());
-
 		return authProvider;
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(10,new SecureRandom());
+		return new BCryptPasswordEncoder(10, new SecureRandom());
 	}
-
 
 	@Bean
 	@Order(1)
 	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
 
 		http.authenticationProvider(authenticationProvider());
-		http.securityMatcher("/api/**").exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+
+		http.securityMatcher("/api/**")
+				.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+
 		http.authorizeHttpRequests(authorize -> authorize
-				//PRODUCTS
-				.requestMatchers(HttpMethod.POST,"/api/products/{id}/imagen").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.POST,"/api/products").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.PUT,"/api/products/{id}").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.DELETE,"/api/products").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.DELETE,"/api/products/{id}").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.DELETE,"/api/products/{id}/imagen").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.POST,"/api/products/purchase").hasAnyRole("USER","ADMIN")
-				.requestMatchers(HttpMethod.GET,"/api/products").hasAnyRole("USER","ADMIN")
-				.requestMatchers(HttpMethod.GET,"/api/products/{id}").hasAnyRole("USER","ADMIN")
-				.requestMatchers(HttpMethod.GET,"/api/products/{id}/imagen").hasAnyRole("USER","ADMIN")
-				//USER
-				.requestMatchers(HttpMethod.GET,"/api/User").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.GET,"/api/User/{username}").hasAnyRole("ADMIN","USER")
-				.requestMatchers(HttpMethod.PUT,"/api/User/{username}").hasAnyRole("ADMIN","USER")
-				.requestMatchers(HttpMethod.DELETE,"/api/User/{username}").hasAnyRole("ADMIN","USER")
-				//COMBATCLASS
-				.requestMatchers(HttpMethod.GET,"/api/combatclass").hasAnyRole("ADMIN","USER")
-				.requestMatchers(HttpMethod.GET,"/api/combatclass/{id}").hasAnyRole("ADMIN","USER")
-				.requestMatchers(HttpMethod.POST,"/api/combatclass").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.DELETE,"/api/combatclass/{id}").hasRole("ADMIN")
-				//MEMBERSHIP
-				.requestMatchers(HttpMethod.GET,"/api/Membership").permitAll()
-				.requestMatchers(HttpMethod.GET,"/api/Membership/{id}").permitAll()
-				.requestMatchers(HttpMethod.POST,"/api/Membership").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.DELETE,"/api/Membership/{id}").hasRole("ADMIN")
-				.requestMatchers(HttpMethod.POST,"/Membership/{id}").hasAnyRole("ADMIN","USER")
+				// PRODUCTOS
+				.requestMatchers(HttpMethod.POST, "/api/products/{id}/imagen").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.POST, "/api/products").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.PUT, "/api/products/{id}").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.DELETE, "/api/products").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.DELETE, "/api/products/{id}").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.DELETE, "/api/products/{id}/imagen").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.POST, "/api/products/purchase").hasAnyRole("USER", "ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/products").hasAnyRole("USER", "ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/products/{id}").hasAnyRole("USER", "ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/products/{id}/imagen").hasAnyRole("USER", "ADMIN")
+				// USUARIOS
+				.requestMatchers(HttpMethod.GET, "/api/User").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.GET, "/api/User/{username}").hasAnyRole("ADMIN", "USER")
+				.requestMatchers(HttpMethod.PUT, "/api/User/{username}").hasAnyRole("ADMIN", "USER")
+				.requestMatchers(HttpMethod.DELETE, "/api/User/{username}").hasAnyRole("ADMIN", "USER")
+				// COMBATCLASS
+				.requestMatchers(HttpMethod.GET, "/api/combatclass").hasAnyRole("ADMIN", "USER")
+				.requestMatchers(HttpMethod.GET, "/api/combatclass/{id}").hasAnyRole("ADMIN", "USER")
+				.requestMatchers(HttpMethod.POST, "/api/combatclass").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.DELETE, "/api/combatclass/{id}").hasRole("ADMIN")
+				// MEMBERSHIP
+				.requestMatchers(HttpMethod.GET, "/api/Membership").permitAll()
+				.requestMatchers(HttpMethod.GET, "/api/Membership/{id}").permitAll()
+				.requestMatchers(HttpMethod.POST, "/api/Membership").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.DELETE, "/api/Membership/{id}").hasRole("ADMIN")
+				.requestMatchers(HttpMethod.POST, "/Membership/{id}").hasAnyRole("ADMIN", "USER")
 				.anyRequest().permitAll());
 
 		http.formLogin(formLogin -> formLogin.disable());
 		http.csrf(csrf -> csrf.disable());
 		http.httpBasic(httpBasic -> httpBasic.disable());
 		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
 		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
+		// Añadir cabeceras de seguridad
+		http.headers(headers -> headers
+				.xssProtection(xss -> xss.headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK))
+				.frameOptions(frameOptions -> frameOptions.deny())
+				.httpStrictTransportSecurity(hsts -> hsts.includeSubDomains(true).preload(true).maxAgeInSeconds(31536000))
+				.contentTypeOptions(Customizer.withDefaults())
+		);
+
 		return http.build();
-
-
 	}
 
 	@Bean
 	@Order(2)
 	public SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
+
+		http.authenticationProvider(authenticationProvider());
+
 		http.headers(headers -> headers
 				.xssProtection(xss -> xss.headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK))
-				.contentSecurityPolicy(cps -> cps.policyDirectives(
+				.frameOptions(frameOptions -> frameOptions.deny())
+				.httpStrictTransportSecurity(hsts -> hsts.includeSubDomains(true).preload(true).maxAgeInSeconds(31536000))
+				.contentTypeOptions(Customizer.withDefaults())
+				.contentSecurityPolicy(csp -> csp.policyDirectives(
 						"default-src 'none'; " +
 								"script-src 'self' " +
 								"'sha256-7CrbOuHXC5pcX5/WZqrJ1wsaWibsKsVvGKmDbneg1MQ=' " +
@@ -143,56 +155,29 @@ public class SecurityConfig{
 				))
 		);
 
-
-		http.authenticationProvider(authenticationProvider());
-
 		http
 				.authorizeHttpRequests(authorize -> authorize
-						// PUBLIC PAGES
-						.requestMatchers(HttpMethod.POST, "/Membership/{id}").hasAnyRole("USER","ADMIN")
-						.requestMatchers("/").permitAll()
-						.requestMatchers("/error").permitAll()
-						.requestMatchers("/Market").permitAll()
-						.requestMatchers("/login").permitAll()
-						.requestMatchers("/AboutUs").permitAll()
-						.requestMatchers("/register").permitAll()
-						.requestMatchers("/Membership").permitAll()
-						.requestMatchers("/Membership/{id}").permitAll()
+						.requestMatchers(HttpMethod.POST, "/Membership/{id}").hasAnyRole("USER", "ADMIN")
+						.requestMatchers("/", "/error", "/Market", "/login", "/AboutUs", "/register", "/Membership", "/Membership/{id}").permitAll()
 						.requestMatchers("/v3/api-docs**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-						// STATIC RESOURCES
-						//.requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
 						.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-						// PRIVATE PAGES
-						.requestMatchers("/products/purchase").hasAnyRole("USER","ADMIN")
-						.requestMatchers("/Market/products/formproducto").hasAnyRole("ADMIN")
+						.requestMatchers("/products/purchase").hasAnyRole("USER", "ADMIN")
+						.requestMatchers("/Market/products/formproducto").hasRole("ADMIN")
 						.requestMatchers("/Market/products/editarproducto/*").hasRole("ADMIN")
 						.requestMatchers("/Market/products/delete/*").hasRole("ADMIN")
 						.requestMatchers("/Admin/combatclass/**").hasRole("ADMIN")
-						.requestMatchers("/Mymemberships").hasRole("ADMIN")
-						.requestMatchers("/Membership/formsuscripcion").hasRole("ADMIN")
-						.requestMatchers("/nuevasuscripcion").hasRole("ADMIN")
-						.requestMatchers("/Membership/{id}/delete").hasRole("ADMIN")
-						.requestMatchers("/Market").hasAnyRole("USER","ADMIN")
-						.requestMatchers("/successPage").hasAnyRole("USER","ADMIN")
-						.requestMatchers("/download/{id}").hasAnyRole("ADMIN","USER")
-						.requestMatchers("/nuevoproducto").hasRole("ADMIN")
-						.requestMatchers("/Market/products/*").hasAnyRole("ADMIN","USER")
-						.requestMatchers("/Market/products").hasAnyRole("ADMIN","USER")
+						.requestMatchers("/Mymemberships", "/Membership/formsuscripcion", "/nuevasuscripcion", "/Membership/{id}/delete").hasRole("ADMIN")
+						.requestMatchers("/Market").hasAnyRole("USER", "ADMIN")
+						.requestMatchers("/successPage", "/download/{id}", "/nuevoproducto").hasAnyRole("USER", "ADMIN")
+						.requestMatchers("/Market/products/*", "/Market/products").hasAnyRole("USER", "ADMIN")
 						.requestMatchers("/Market/product/**").hasRole("ADMIN")
-						.requestMatchers("/private/delete/*").hasAnyRole("ADMIN","USER")
-						.requestMatchers("/private/{id}").hasAnyRole("USER","ADMIN")
-						.requestMatchers("/private").hasAnyRole("USER","ADMIN")
-						.requestMatchers("/Admin").hasRole("ADMIN")
-						.requestMatchers("/Admin/combatclass").hasRole("ADMIN")
-						.requestMatchers("/Admin/combatclass/{id}").hasRole("ADMIN")
-						.requestMatchers("/nuevaclase").hasRole("ADMIN")
-						.requestMatchers("/Admin/combatclass/formcombatclass").hasRole("ADMIN")
-						.requestMatchers("/Admin/combatclass/{id}/delete").hasRole("ADMIN")
+						.requestMatchers("/private/delete/*", "/private/{id}", "/private").hasAnyRole("USER", "ADMIN")
+						.requestMatchers("/Admin", "/Admin/combatclass", "/Admin/combatclass/{id}", "/nuevaclase", "/Admin/combatclass/formcombatclass", "/Admin/combatclass/{id}/delete").hasRole("ADMIN")
 				)
 				.formLogin(formLogin -> formLogin
 						.loginPage("/login")
 						.failureUrl("/error")
-						.defaultSuccessUrl("/private",true)
+						.defaultSuccessUrl("/private", true)
 						.permitAll()
 				)
 				.sessionManagement(sessionManagement -> sessionManagement
@@ -212,5 +197,4 @@ public class SecurityConfig{
 
 		return http.build();
 	}
-
 }
